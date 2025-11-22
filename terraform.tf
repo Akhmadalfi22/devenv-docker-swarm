06171f5f3c9db408e92ede5abd9bbb4bc0c7e997
Ivan Gogol <>
2025-11-22 09:56:52 +0000

    chore: update terraform and providers

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,17 @@
 terraform {
-  required_version = ">= 1.12"
+  required_version = ">= 1.14" // https://github.com/hashicorp/terraform/releases
   required_providers {
     external = {
-      source  = "hashicorp/external"
+      source  = "hashicorp/external" // https://registry.terraform.io/providers/hashicorp/external/latest
       version = "~> 2.3"
     }
     random = {
-      source  = "hashicorp/random"
+      source  = "hashicorp/random" // https://registry.terraform.io/providers/hashicorp/random/latest
       version = "~> 3.7.2"
     }
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.81.0"
+      source  = "hashicorp/aws" // https://registry.terraform.io/providers/hashicorp/aws/latest
+      version = "6.22.1"
     }
   }
 }
